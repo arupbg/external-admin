b08602ddb7fe36b547ec5f8c22660361c013655d
arup-banerjee <arup@alanaamy.net>
2018-02-22 11:37:06 +0000

    addded environment config

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,9 @@
-'use strict';
-
-module.exports = function(/* environment, appConfig */) {
-  return { };
+/*jshint node:true*/ 
+'use strict';  
+module.exports = function(environment) {   
+	const ENV = {     
+		modulePrefix: 'external-admin',     
+		environment: environment   
+	}    
+	return ENV; 
 };
